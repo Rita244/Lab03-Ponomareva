78df4dd0adc2149825f65b4a036e805715a6edd0
Рита Пономарёва <ponomarevarita11@gmail.com>
2026-09-13 18:57:00 +0300

    Lab03: compound assignment operators

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Банковский счёт");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500;
+Console.WriteLine($"После пополения на 500: {balance}");
+
+balance -= 200;
+Console.WriteLine($"После покупки на 200: {balance}");
+
+balance *= 1.05;
+Console.WriteLine($"После начисления на 5%: {balance}");
+
+balance /= 2;
+Console.WriteLine($"После деления пополам: {balance}");
