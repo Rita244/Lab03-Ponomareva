f60bcf10454ced8063d1f77f6299ca67b78b6afc
Рита Пономарёва <ponomarevarita11@gmail.com>
2026-09-13 19:47:23 +0300

    Исправлена ошибка с бесконечной папкой img

--- Program.cs
+++ Program.cs
@@ -1,18 +1,37 @@
-﻿// Console.WriteLine("Hello, World!");
+﻿// // Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Банковский счёт");
+// Console.WriteLine("Банковский счёт");
 
-double balance = 1000;
-Console.WriteLine($"Начальный баланс: {balance}");
+// double balance = 1000;
+// Console.WriteLine($"Начальный баланс: {balance}");
 
-balance += 500;
-Console.WriteLine($"После пополения на 500: {balance}");
+// balance += 500;
+// Console.WriteLine($"После пополения на 500: {balance}");
 
-balance -= 200;
-Console.WriteLine($"После покупки на 200: {balance}");
+// balance -= 200;
+// Console.WriteLine($"После покупки на 200: {balance}");
 
-balance *= 1.05;
-Console.WriteLine($"После начисления на 5%: {balance}");
+// balance *= 1.05;
+// Console.WriteLine($"После начисления на 5%: {balance}");
 
-balance /= 2;
-Console.WriteLine($"После деления пополам: {balance}");
+// balance /= 2;
+// Console.WriteLine($"После деления пополам: {balance}");
+
+Console.WriteLine();
+Console.WriteLine("Постфикс vs префикс");
+
+int lessonNumber = 1;
+Console.WriteLine($"lessonNumber++ выводит: {lessonNumber++}");
+Console.WriteLine($"После этого lessonNumber = {lessonNumber}");
+
+int weekNumber = 1;
+Console.WriteLine($"++weekNumber выводит: {++weekNumber}");
+Console.WriteLine($"После этого weekNumber = {weekNumber}");
+
+Console.WriteLine();
+Console.WriteLine("Практическая ловушка");
+
+int attempts = 0;
+Console.WriteLine($"Попытка № {++attempts}");
+Console.WriteLine($"Попытка № {++attempts}");
+Console.WriteLine($"Всего попыток: {++attempts}");
